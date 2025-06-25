1d293ee104541dab23a5c865c77f45ab6f8ac94a
Nirmalkumar59 <nirmalkumar50687@gmail.com>
2025-06-25 08:25:52 +0530

    Feat : Add Function Input

--- sampleFirst.py
+++ sampleFirst.py
@@ -2,3 +2,6 @@
 from datetime import datetime
 def sample(name)->str:
     return f"Hello, {name}! , DateTime :{datetime.now()}"
+if __name__ == "__main__":
+    name = input("Enter The Name:")
+    print(sample(name))
